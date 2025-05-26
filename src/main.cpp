b82b771b17c4c28cc251fd9c198a865be3dffc2a
Mike Wessling <mwessling@gmail.com>
2025-05-26 17:39:04 +0200

    tinkering

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <ezButton.h>
 
 // put function declarations here:
+void update_ripems();
 float revsPerMinute();
 void RPMLED(float);
 
@@ -10,10 +11,11 @@ void RPMLED(float);
 const int input_RPM_pin = 14;
 ezButton rpm_button(input_RPM_pin);
 
-const int timeout = 2500;
-unsigned long last_switch_time;
-unsigned long last_isr;
-unsigned long switch_diff;
+const int idle_time = 2500;
+unsigned long last_falling_edge_time;
+bool was_pressed = false;
+float rpm = 0;
+float last_rpm = 0.0;
 
 // RPM light
 const int output_RPM_led = 15;
@@ -37,11 +39,10 @@ void setup() {
   pinMode(output_RPM_led, OUTPUT);
 
 	// attachInterrupt(input_RPM_pin, isr, RISING);
-  rpm_button.setDebounceTime();
-  last_switch_time = 0;
-  last_isr = 0;
-  switch_diff = 0;
+  rpm_button.setDebounceTime(1);
+  rpm_button.resetCount();
 
+  last_falling_edge_time = millis();
   last_led_time = millis();
   digitalWrite(output_RPM_led, HIGH);
   RPM = 0;
@@ -51,53 +52,48 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  update_ripems();
+  if (! fabs(rpm - last_rpm) < 0.00001){
+    Serial.printf("RPM: %f\n",rpm);
+    last_rpm = rpm;
+  }
+  unsigned int sensor = analogRead(GPIO_NUM_36);
+  if (sensor < 1700 or sensor  > 2000)
+      Serial.printf("HB %u\n",sensor);
+}
+
+void update_ripems() {
+  unsigned long revtime;
+  unsigned long now = millis();
   rpm_button.loop(); // MUST call the loop() function first
-
-  if (rpm_button.isPressed())
-  {
-    // Get timings
-    unsigned long current_time = millis();
-    switch_diff = current_time - last_switch_time;
-    last_switch_time = current_time;
-
-    // Trigger LED
+  // if there is no button press in the idle time assume it is not rotating
+  if (now - last_falling_edge_time > idle_time) {
+    rpm = 0.0;
+  }
+  if (rpm_button.isPressed() && ! was_pressed) {
+    was_pressed = true;
     digitalWrite(output_RPM_led, HIGH);
-    last_led_time = current_time;
   }
-
-  if (last_switch_time != 0)
-  {
-    RPM = revsPerMinute();
-    RPMLED(RPM);
-
-    uint32_t freq = pow(RPM, 2) / 5;
-    if (freq > 10 || freq < 20000) {
-      Serial.printf("%i \n", freq);
-      dac1.outputCW(freq);
-      dac2.outputCW(freq);
-    }
-  }
-
-  // sleep(0.3);
-}
-
-float revsPerMinute() {
-  // Check to see if stopped
-  if (millis() - last_switch_time > timeout) {
-    switch_diff = 0;
-    last_switch_time = 0;
-    return 0;
-  } else {
-    return 60000.0 / (int)switch_diff;
-  }
-}
-
-void RPMLED(float RPM) {
-  if (RPM == 0) return;
-
-  // If it's been long enough, turn off the LED
-  if (millis() - last_led_time > switch_diff / 2.0) {
+  // on down edge.
+  if (rpm_button.isReleased() && was_pressed){
+    // Serial.printf("Off at %lu ms\n",now);
+    revtime = now - last_falling_edge_time;
+    // Serial.printf("revtime=%lu ms\n",revtime);
+    rpm = 60000.0 / float(revtime) ;
+    was_pressed = false;
+    last_falling_edge_time = now;
     digitalWrite(output_RPM_led, LOW);
   }
+
+
 }
+
+// void RPMLED(float RPM) {
+//   if (RPM == 0) return;
+
+//   // If it's been long enough, turn off the LED
+//   if (millis() - last_led_time > switch_diff / 2.0) {
+//     digitalWrite(output_RPM_led, LOW);
+//   }
+// }
